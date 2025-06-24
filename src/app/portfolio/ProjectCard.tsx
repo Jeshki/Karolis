6af128b307b3a353c81c24d0f1952807e0f3c1bc
Karolis Cibiras <83600971+Jeshki@users.noreply.github.com>
2025-06-24 19:55:17 +0300

    Update ProjectCard.tsx

--- src/app/portfolio/ProjectCard.tsx
+++ src/app/portfolio/ProjectCard.tsx
@@ -56,7 +56,7 @@ export const ProjectCard = ({ title, imageUrl, tags, liveUrl }: ProjectCardProps
   const { t } = useTranslation(); // Inicializuokite hook'ą
 
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden transition-transform transform hover:-translate-y-2">
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:-translate-y-2">
       <Image src={imageUrl} alt={title} width={500} height={300} className="w-full h-48 object-cover" />
       <div className="p-6 text-center">
         <div className="flex space-x-2 mb-2 justify-center">
@@ -66,7 +66,7 @@ export const ProjectCard = ({ title, imageUrl, tags, liveUrl }: ProjectCardProps
             </div>
           ))}
         </div>
-        <h3 className="text-xl font-bold text-gray-800 dark:text-white mb-4">{title}</h3>
+        <h3 className="text-xl font-bold text-white mb-4">{title}</h3>
 
         <div className="flex justify-center">
           {liveUrl && (
