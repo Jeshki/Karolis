bf3bb66dcec7253562afeee260a5f5004565d212
Karolis Cibiras <83600971+Jeshki@users.noreply.github.com>
2025-06-24 20:17:59 +0300

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -25,8 +25,8 @@ export const Header = () => {
         
         <div className="text">
           <h1 className="text-4xl md:text-5xl font-bold tracking-wider">{t('homepage.name')}</h1>
-          <p className="text-xl md:text-2xl text-teal-300 mt-2">{t('cv_page.frontend_developer_title')}</p> {/* Pridėta nauja vertimo eilutė */}
-          <div className="mt-6 flex flex-wrap justify-center gap-x-6 gap-y-3 text-gray-300 dark:text-gray-400">
+          <p className="text-xl md:text-2xl text-teal-300 mt-2">{t('cv_page.frontend_developer_title')}</p>
+          <div className="mt-6 flex flex-wrap justify-center gap-x-6 gap-y-3 text-gray-300">
             <a href="tel:+37060302903" className="flex items-center hover:text-teal-300 transition-colors">
               <PhoneIcon className="h-5 w-5 mr-2" aria-hidden="true" /> +370 603 02903
             </a>
@@ -34,7 +34,7 @@ export const Header = () => {
               <EnvelopeIcon className="h-5 w-5 mr-2" aria-hidden="true" /> karoliscibiras@gmail.com
             </a>
             <div className="flex items-center">
-              <MapPinIcon className="h-5 w-5 mr-2" aria-hidden="true" /> {t('contact_page.city_name')} {/* Pridėta nauja vertimo eilutė */}
+              <MapPinIcon className="h-5 w-5 mr-2" aria-hidden="true" /> {t('contact_page.city_name')}
             </div>
           </div>
         </div>
